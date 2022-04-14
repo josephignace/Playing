701ac8ffaef608c4b821fc41d0f7c1947a21f215
Jose Ignacio Cuitun Coronado <61969236+josephignace@users.noreply.github.com>
2022-04-14 17:01:16 +0100

    Adding-Variables

--- analysis/Report.R
+++ analysis/Report.R
@@ -1,10 +1,12 @@
 library('tidyverse')
+library('dplyr')
 
 df_input <- read_csv(
     here::here("output", "input.csv"),
-    col_types = cols(patient_id = col_integer(), 
-                    age = col_double())
-)
+    col_types = cols(patient_id = col_integer(), age = col_double(), sex = col_double(), region = col_double())
+)     
+
+df_input <- df_input %>% mutate(Gender.Dummy = if_else(sex == "M", 0, 1))
 
 plot_age <- ggplot(data = df_input, aes(df_input$age)) + 
                     geom_histogram()
@@ -13,3 +15,10 @@ ggsave(
     plot = plot_age,
     filename = "Descriptive-Age.png", path = here::here("output"),
 )                    
+
+#plot_sex <- ggplot(data = df_input, aes(df_input$sex)) + geom_histogram()
+#
+#ggsave(
+#    plot = plot_sex,
+#    filename = "Descriptive-Sex.png", path = here::here("output"),
+#)
--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,5 +1,7 @@
 from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
 
+import numpy as np
+np.random.seed(123456)
 
 study = StudyDefinition(
     default_expectations={
@@ -16,6 +18,33 @@ study = StudyDefinition(
         return_expectations={
             "rate": "universal",
             "int": {"distribution": "population_ages"},
+        },
+    ),
+
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+        }
+    ),
+
+    region=patients.registered_practice_as_of(
+        "2020-02-01",
+        returning="nuts1_region_name",
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "North East": 0.1,
+                    "North West": 0.1,
+                    "Yorkshire and the Humber": 0.1,
+                    "East Midlands": 0.1,
+                    "West Midlands": 0.1,
+                    "East of England": 0.1,
+                    "London": 0.2,
+                    "South East": 0.2,
+                },
+            },
         },
     ),
 )
